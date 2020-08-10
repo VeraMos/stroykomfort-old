2fda88b2c6e03b4349f7f476356b00e771651a48
Vera <verkaevg@.gmail.com>
2020-08-10 11:44:29 +0400

    block workes

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -39,5 +39,21 @@ $(document).ready(function () {
     });
   });
 
+   $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      //dots: true,
+      centerMode: true,
+      focusOnSelect: true
+    });
+
 
   });
